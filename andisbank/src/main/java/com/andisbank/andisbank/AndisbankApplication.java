8971c347589159414f1d241cf69a88946e24b65e
Lucas Giffuni <55067106+LucasGiffuni@users.noreply.github.com>
2024-09-04 18:49:47 -0300

    initial commit

---
+++ andisbank/src/main/java/com/andisbank/andisbank/AndisbankApplication.java
@@ -0,0 +1,13 @@
+package com.andisbank.andisbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndisbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndisbankApplication.class, args);
+	}
+
+}
